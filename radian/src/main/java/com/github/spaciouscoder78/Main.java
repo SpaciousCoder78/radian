c40dd52f09c108b2d1f6967bd7aca1ee37d1affe
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2023-11-04 08:48:14 +0530

    basic terminal window

--- radian/src/main/java/com/github/spaciouscoder78/Main.java
+++ radian/src/main/java/com/github/spaciouscoder78/Main.java
@@ -1,7 +1,16 @@
 package com.github.spaciouscoder78;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)  {
+        try{
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
